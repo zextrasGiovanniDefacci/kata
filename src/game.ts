8c03706cbd2f87081d91d996b124bc508e0729b1
cataldo <cataldo.mazzilli@zextras.com>
2026-02-12 15:41:53 +0100

    test: update game tests to use character IDs and add self-damage check

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,26 @@
-export const DEFAULT_CHARACTER: Character = {
+export const DEFAULT_CHARACTER = {
     health: 1000,
     level: 1,
-};
+} as const;
 
 export type Character = {
     health: number
     level: number
+    id: string
 }
 
-export function createCharacter(): Character {
-    return {...DEFAULT_CHARACTER};
+export function createCharacter(value: Omit<Character, 'id'> = DEFAULT_CHARACTER): Character {
+    return {...value, id: crypto.randomUUID()};
 }
 
 export function isAlive({health}:Character):boolean {
     return health > 0; 
 }
 
-export function dealDamage(targetCharacter:Character, damage:number):Character {
+export function dealDamage(sourceCharacter: Character, targetCharacter:Character, damage:number):Character {
+    if(sourceCharacter.id === targetCharacter.id) {
+        return targetCharacter;
+    }
     const health = targetCharacter.health - damage;
     return {
         ...targetCharacter,
